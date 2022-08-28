280d076fedc939f5d45cfe0419b74a69503cf6f2
jstutul <jaforsadiktutul9228@gmail.com>
2022-08-28 16:21:47 +0600

    first commit

---
+++ src/components/main/Main.js
@@ -0,0 +1,147 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div id="content-wrapper" className="d-flex flex-column">
+        <div id="content">
+            <nav className="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+                <button id="sidebarToggleTop" className="btn btn-link d-md-none rounded-circle mr-3">
+                    <i className="fa fa-bars"></i>
+                </button>
+                
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item dropdown no-arrow">
+                        <a className="nav-link dropdown-toggle" href="@#" id="userDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <span className="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                            <img className="img-profile rounded-circle"
+                                src="img/undraw_profile.svg" alt='text' />
+                        </a>
+                        <div className="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                            aria-labelledby="userDropdown">
+                            <a className="dropdown-item" href="@#" data-toggle="modal" data-target="#logoutModal">
+                                <i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                Logout
+                            </a>
+                        </div>
+                    </li>
+
+                </ul>
+
+            </nav>
+            <div className="container-fluid">
+
+                <div className="d-sm-flex align-items-center justify-content-between mb-4">
+                    <h1 className="h3 mb-0 text-gray-800">Dashboard</h1>
+                    <a href="@#" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                            className="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+                </div>
+
+                <div className="row">
+
+                    <div className="col-xl-3 col-md-6 mb-4">
+                        <div className="card border-left-primary shadow h-100 py-2">
+                            <div className="card-body">
+                                <div className="row no-gutters align-items-center">
+                                    <div className="col mr-2">
+                                        <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                            Earnings (Monthly)</div>
+                                        <div className="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                                    </div>
+                                    <div className="col-auto">
+                                        <i className="fas fa-calendar fa-2x text-gray-300"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-xl-3 col-md-6 mb-4">
+                        <div className="card border-left-success shadow h-100 py-2">
+                            <div className="card-body">
+                                <div className="row no-gutters align-items-center">
+                                    <div className="col mr-2">
+                                        <div className="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                            Earnings (Annual)</div>
+                                        <div className="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
+                                    </div>
+                                    <div className="col-auto">
+                                        <i className="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-xl-3 col-md-6 mb-4">
+                        <div className="card border-left-info shadow h-100 py-2">
+                            <div className="card-body">
+                                <div className="row no-gutters align-items-center">
+                                    <div className="col mr-2">
+                                        <div className="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
+                                        </div>
+                                        <div className="row no-gutters align-items-center">
+                                            <div className="col-auto">
+                                                <div className="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
+                                            </div>
+                                            <div className="col">
+                                                <div className="progress progress-sm mr-2">
+                                                    <div className="progress-bar bg-info" role="progressbar"
+                                                        style={{width: "50%"}} aria-valuenow="50" aria-valuemin="0"
+                                                        aria-valuemax="100"></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-auto">
+                                        <i className="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-xl-3 col-md-6 mb-4">
+                        <div className="card border-left-warning shadow h-100 py-2">
+                            <div className="card-body">
+                                <div className="row no-gutters align-items-center">
+                                    <div className="col mr-2">
+                                        <div className="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                            Pending Requests</div>
+                                        <div className="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                                    </div>
+                                    <div className="col-auto">
+                                        <i className="fas fa-comments fa-2x text-gray-300"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-lg-6 mb-4">
+
+                        
+
+                    </div>
+
+                    <div className="col-lg-6 mb-4">
+                       
+
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+        <footer className="sticky-footer bg-white">
+            <div className="container my-auto">
+                <div className="copyright text-center my-auto">
+                    <span>Copyright &copy;Hotel Booking</span>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    );
+};
+
+export default Main;
