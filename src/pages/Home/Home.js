ac74e7e1bf0f99952228c5a026eac963a8460a44
jstutul <jaforsadiktutul9228@gmail.com>
2022-08-28 16:36:17 +0600

    first commit

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,23 +8,23 @@ const Hotel = () => {
             <Sidebar/>
             <Main/>
         </div>
-        <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fas fa-angle-up"></i>
+        <a className="scroll-to-top rounded" href="#page-top">
+        <i className="fas fa-angle-up"></i>
     </a>
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    <div className="modal fade" id="logoutModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+        <div className="modal-dialog" role="document">
+            <div className="modal-content">
+                <div className="modal-header">
+                    <h5 className="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <button className="close" type="button" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">×</span>
                     </button>
                 </div>
-                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-                <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.html">Logout</a>
+                <div className="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div className="modal-footer">
+                    <button className="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a className="btn btn-primary" href="login.html">Logout</a>
                 </div>
             </div>
         </div>
