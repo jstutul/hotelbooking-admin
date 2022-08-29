df8e19a31cd8ebabc9327221cd150f82126d63f4
jstutul <jaforsadiktutul9228@gmail.com>
2022-08-30 02:56:55 +0600

    admin

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import { useContext } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home.js"
-import {AuthContext} from "./context/AuthContext.js"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home/Home.js";
+import { AuthContext } from "./context/AuthContext.js";
+import Login from "./pages/login/Login";
 function App() {
-  const ProtectedRoute=({children})=>{
-    const {user}=useContext(AuthContext)
-  }
+  const ProtectedRoute = ({ children }) => {
+    const { user } = useContext(AuthContext);
+    console.log(user);
+    if (!user) {
+      return <Navigate to="/login" />;
+    }
+    return children;
+  };
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        ></Route>
+        <Route path="/login" element={<Login />}></Route>
       </Routes>
     </BrowserRouter>
   );
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,46 +1,79 @@
-import React, { useContext, useState } from 'react';
-import "./Login.css"
-import {AuthContext} from "../../context/AuthContext"
+import React, { useContext, useState } from "react";
+import "./Login.css";
+import { AuthContext } from "../../context/AuthContext";
 import axios from "axios";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 const Login = () => {
-    const {user,loading,error,dispatch}=useContext(AuthContext)
-    const nagivate=useNavigate();
-    const [credential,setCredential]=useState({
-        username:undefined,
-        password:undefined,
-    })
-    const handleChange=(e)=>{
-        setCredential((prev)=>({...prev,[e.target.id]:e.target.value}) )
+  const { user, loading, error, dispatch } = useContext(AuthContext);
+  const nagivate = useNavigate();
+  const [credential, setCredential] = useState({
+    username: undefined,
+    password: undefined,
+  });
+  const handleChange = (e) => {
+    setCredential((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGIN_START" });
+    try {
+      const res = await axios.post(
+        "http://localhost:5000/api/auth/login",
+        credential
+      );
+      if (res.data.isAdmin) {
+        dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
+        nagivate("/");
+      } else {
+        dispatch({
+          type: "LOGIN_FAILURE",
+          payload: { message: "you are not allow in Admin Panel" },
+        });
+      }
+    } catch (err) {
+      dispatch({ type: "LOGIN_FAILURE", payload: err.response.data });
     }
-    const handleLogin=async e=>{
-        e.preventDefault();
-        dispatch({type:"LOGIN_START"});
-        try{
-            const res=await axios.post('http://localhost:5000/api/auth/login',credential);
-            dispatch({type:"LOGIN_SUCCESS",payload:res.data});
-            nagivate("/");
-        }catch(err){
-            dispatch({type:"LOGIN_FAILURE",payload:err.response.data})
-        }
-    }
+  };
 
-    return (
-        <div className='login-container'>
-            <div className='login'>
-                <h2 id="login-title">Login Page</h2>
-                <label><b>User Name</b> </label> 
-                <input type="text" name="username" id="username" onChange={handleChange}/>
-                <br/><br/>    
-                <label><b>Password </b></label> 
-                <input type="password" name="password" id="password" onChange={handleChange}/>
-                <br/><br/> 
-                <input type="button" disabled={loading} onClick={handleLogin} value="Login" className='lbutton'/>
-                <br/><br/>
-                {error && <span id="login-span"> {error.message}</span>}
-            </div>
-        </div>
-    );
+  return (
+    <div className="login-container">
+      <div className="login">
+        <h2 id="login-title">Login Page</h2>
+        <label>
+          <b>User Name</b>{" "}
+        </label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          onChange={handleChange}
+        />
+        <br />
+        <br />
+        <label>
+          <b>Password </b>
+        </label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          onChange={handleChange}
+        />
+        <br />
+        <br />
+        <input
+          type="button"
+          disabled={loading}
+          onClick={handleLogin}
+          value="Login"
+          className="lbutton"
+        />
+        <br />
+        <br />
+        {error && <span id="login-span"> {error.message}</span>}
+      </div>
+    </div>
+  );
 };
 
 export default Login;
